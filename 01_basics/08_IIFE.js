9ced58657c527e6cc2714ba8583db3269adf9774
Krupasindhu-dev <srikrupasindhu@gmail.com>
2025-04-15 04:14:34 +0000

    revised IIFE

--- 01_basics/08_IIFE.js
+++ 01_basics/08_IIFE.js
@@ -1,11 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
+// To avoid pollution from Global scope .And we want to execute it immediately.
+// place within parenthesis to protect from pollution of glaobal scope
 
-
-(function chai(){
+(function chai(){    
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); // using ; is mandatory in IIFE to let compiler know that here this IIFE ends, i.e where to stop the context
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('hitesh');   // passing argument
